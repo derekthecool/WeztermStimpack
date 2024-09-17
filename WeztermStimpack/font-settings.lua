6a67a64ca7e59775256ba7553a0622cd02f9deae
Derek Lomax <derekthecool@gmail.com>
2024-09-17 10:23:16 -0600

    feat(font): stop using local custom fonts, too many problems
    Many nerd font icons do not work and other trouble

--- WeztermStimpack/font-settings.lua
+++ WeztermStimpack/font-settings.lua
@@ -5,15 +5,16 @@ local M = {}
 ---@param config table
 M.init = function(config)
     config.font_size = 14
-    config.font_dirs = { 'fonts' }
-    config.font_locator = 'ConfigDirsOnly'
-
     -- config.font = wezterm.font('JetBrains Mono')
-    config.font = wezterm.font_with_fallback({
-        'Hermit',
-        'Hack Nerd Font',
-        'Symbols Nerd Font Mono',
-    })
+
+    -- config.font_dirs = { 'fonts' }
+    -- config.font_locator = 'ConfigDirsOnly'
+    --
+    -- config.font = wezterm.font_with_fallback({
+    --     'Hermit',
+    --     'Hack Nerd Font',
+    --     'Symbols Nerd Font Mono',
+    -- })
 
     -- Enable ligatures
     config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
