dc49b9e3ee68470f80d3331de05a3356025cfbd2
Derek Lomax <derekthecool@gmail.com>
2024-12-17 20:13:00 -0700

    feat: move config table higher in wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,12 @@
+local config = {}
+
+-- Use pwsh as default shell
+config.default_prog = { 'pwsh' }
+config.exit_behavior = 'CloseOnCleanExit'
+
 -- My configuration WeztermStimpack
 require('WeztermStimpack.right-status-format')
 require('WeztermStimpack.gui-startup')
-
-local config = {}
 
 require('WeztermStimpack.bell-settings').init(config)
 require('WeztermStimpack.font-settings').init(config)
@@ -13,15 +17,11 @@ config.window_close_confirmation = 'NeverPrompt'
 config.scrollback_lines = 5000
 
 -- Visual settings
-config.color_scheme = 'Atelier Sulphurpool (base16)'                 -- Same as my neovim config
+config.color_scheme = 'Atelier Sulphurpool (base16)' -- Same as my neovim config
 config.window_background_opacity = 0.72
-config.window_decorations = 'RESIZE'                                 -- No title bar, but allow window resize with mouse
+config.window_decorations = 'RESIZE' -- No title bar, but allow window resize with mouse
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 } -- No padding
 config.show_update_window = false
-
--- Use pwsh as default shell
-config.default_prog = { 'pwsh' }
-config.exit_behavior = 'CloseOnCleanExit'
 
 -- Easy picks for steno keyboard
 config.quick_select_alphabet = 'abcdefghijklmnopqrstuvwxy'
